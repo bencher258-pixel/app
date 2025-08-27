a62266c723048f8666139029fca70ec5a212d6b5
bencher258-pixel <bencher258@gmail.com>
2025-08-27 13:39:36 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -58,7 +58,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["https://secverse-frontend.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
